d3a950bb1b4a7f8f467448f42d93460e4f281e99
“teji1998” <tejasvinirpk@gmail.com>
2020-07-03 20:17:20 +0530

    Added:Code for employee attendance.

--- EmpWage.java
+++ EmpWage.java
@@ -1,9 +1,22 @@
+import java.util.Random;
 public class EmpWage{
 	public void getWelcomeMessage(){
 		System.out.println("Welcome to the employee wage computation");
 	}
+	public void getAttendance(){
+	Random random = new Random();
+	int empCheck=random.nextInt(2);
+	if(empCheck==1){
+		System.out.println("Employee present");
+		}
+	else{
+		System.out.println("Employee absent");
+		}
+	}
 	public static void main(String[] args){
 	EmpWage message =new EmpWage();
 	message.getWelcomeMessage();
+	EmpWage rand = new EmpWage();
+	rand.getAttendance();
 	}
 }
