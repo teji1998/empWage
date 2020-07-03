bbb538c1a9f17fda1506cba0d23aa8726dae91bc
“teji1998” <tejasvinirpk@gmail.com>
2020-07-03 19:56:31 +0530

    Welcome Message

---
+++ EmpWage.java
@@ -0,0 +1,9 @@
+public class EmpWage{
+	public void getWelcomeMessage(){
+		System.out.println("Welcome to the employee wage computation");
+	}
+	public static void main(String[] args){
+	EmpWage message =new EmpWage();
+	message.getWelcomeMessage();
+	}
+}
