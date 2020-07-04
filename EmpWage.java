451fd7429bfb113374a1fdf22b71f5ecd79398f6
“teji1998” <tejasvinirpk@gmail.com>
2020-07-04 20:16:23 +0530

    Refactored:Using methods

--- EmpWage.java
+++ EmpWage.java
@@ -1,39 +1,51 @@
 import java.util.Random;
 public class EmpWage{
-	static final int ratePerHour=20;
-	static final int fullTime=1;
-	static final int partTime=2;
-	static final int workDays=20;
-	static final int maxHours=100;
-	public void getTotalSal(){
+  	private static int ratePerHour;
+	private static int fullTime=1;
+	private static int partTime=2;
+	private static int maxWorkDays;
+	private static int fullHrs;
+	private static int partHrs;
+	private static int maxHours;
+	public EmpWage( int ratePerHour, int fullHrs, int partHrs, int maxWorkDays,int maxHours){
+		this.ratePerHour = ratePerHour;
+     		this.fullHrs = fullHrs;
+		this.partHrs = partHrs;
+		this.maxWorkDays = maxWorkDays;
+		this.maxHours = maxHours;
+	}
+
+	public int getEmpHrs(){
 	Random random = new Random();
-	int empHour=0;
-	int totalHrs=0;
-	int diff=0;
-	int totalWorkDays=0;
-	while(totalHrs <= maxHours && totalWorkDays <= workDays) {
-	totalWorkDays++;
 	int empCheck=random.nextInt(3);
 	switch(empCheck){
-	case 1: empHour=8;
-		break;
-	case 2: empHour=4;
-		break;
-	default: empHour=0;
+	case 1: 
+		return fullHrs;
+	case 2: 
+		return partHrs;
 	}
-	System.out.println("Employee hours is " +empHour);
-	System.out.println("Daily Wage="+(empHour*EmpWage.ratePerHour));
-	totalHrs+=empHour;
-	if(totalHrs>100);
-		diff=totalHrs-100;
-		empHour=diff;	
-	}
-	System.out.println("Day "+ totalWorkDays + " Hours: " + totalHrs );
-	System.out.println("Total Salary= "+(totalHrs*EmpWage.ratePerHour));
-	}
+	 return 0;
+     }
+	   public int getTotalEmpSalary(){
+		int totalWage = 0;
+		int workingDays = 0;
+		int workingHrs = 0;
+		while (workingDays < maxWorkDays && workingHrs < maxHours){
+   			int empHrs = getEmpHrs();
+     			int dailyWage = 0;
+      			dailyWage = ratePerHour * empHrs;
+			totalWage = totalWage + dailyWage;
+			++workingDays;
+			workingHrs = workingHrs + empHrs;
+		}
+		if (workingHrs > maxHours)
+			return ratePerHour * maxHours;
+      		return totalWage;
+   	}
 	public static void main(String[] args){
-	System.out.println("Welcome to the Employee Wage Computation");
-	EmpWage totalSal = new EmpWage();
-	totalSal.getTotalSal();
+   		System.out.println("Welcome to Employee Wage..");
+    		EmpWage totalSal = new EmpWage( 20, 8, 4, 20, 100);
+		totalSal.getTotalEmpSalary();
+		System.out.println("Total Salary="+ totalSal.getTotalEmpSalary());	
 	}
 }
