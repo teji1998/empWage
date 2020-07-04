825b4122b69342734521be40759b681314c8f130
“teji1998” <tejasvinirpk@gmail.com>
2020-07-04 19:48:02 +0530

    Added:Monthly wage of employee

--- EmpWage.java
+++ EmpWage.java
@@ -3,9 +3,12 @@ public class EmpWage{
 	static final int ratePerHour=20;
 	static final int fullTime=1;
 	static final int partTime=2;
-	public void getDailyWage(){
+	static final int workDays=20;
+	public void getMonthlyWage(){
 	Random random = new Random();
 	int empHour=0;
+	int totalHrs=0;
+	for(int i=0; i<workDays; i++) {
 	int empCheck=random.nextInt(3);
 	switch(empCheck){
 	case 1: empHour=8;
@@ -16,10 +19,13 @@ public class EmpWage{
 	}
 	System.out.println("Employee hours is " +empHour);
 	System.out.println("Daily Wage="+(empHour*EmpWage.ratePerHour));
+	totalHrs+=empHour;
+	}
+	System.out.println("Monthly Wage= "+(totalHrs*EmpWage.ratePerHour));
 	}
 	public static void main(String[] args){
 	System.out.println("Welcome to the Employee Wage Computation");
-	EmpWage dailywage = new EmpWage();
-	dailywage.getDailyWage();
+	EmpWage monthlywage = new EmpWage();
+	monthlywage.getMonthlyWage();
 	}
 }
