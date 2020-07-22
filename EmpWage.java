79de1084986ba15083ec736dd22ff56eec247bc5
“teji1998” <tejasvinirpk@gmail.com>
2020-07-22 18:24:29 +0530

    Added:Functionality for various companies

--- EmpWage.java
+++ EmpWage.java
@@ -1,18 +1,20 @@
 import java.util.Random;
 public class EmpWage{
-  	private static int ratePerHour;
-	private static int fullTime=1;
-	private static int partTime=2;
-	private static int maxWorkDays;
-	private static int fullHrs;
-	private static int partHrs;
-	private static int maxHours;
-	public EmpWage( int ratePerHour, int fullHrs, int partHrs, int maxWorkDays,int maxHours){
+  	private final int ratePerHour;
+	private final int fullTime=1;
+	private final int partTime=2;
+	private final int maxWorkDays;
+	private final int fullHrs;
+	private final int partHrs;
+	private final int maxHours;
+	private final String company;
+	public EmpWage(String company, int ratePerHour, int fullHrs, int partHrs, int maxWorkDays,int maxHours){
 		this.ratePerHour = ratePerHour;
      		this.fullHrs = fullHrs;
 		this.partHrs = partHrs;
 		this.maxWorkDays = maxWorkDays;
 		this.maxHours = maxHours;
+		this.company=company;
 	}
 
 	public int getEmpHrs(){
@@ -42,10 +44,14 @@ public class EmpWage{
 			return ratePerHour * maxHours;
       		return totalWage;
    	}
-	public static void main(String[] args){
-   		System.out.println("Welcome to Employee Wage..");
-    		EmpWage totalSal = new EmpWage( 20, 8, 4, 20, 100);
-		totalSal.getTotalEmpSalary();
-		System.out.println("Total Salary="+ totalSal.getTotalEmpSalary());	
+	   public static void main(String[] args){
+	   		System.out.println("Welcome to Employee Wage..");
+	    		EmpWage empDMart = new EmpWage("DMart", 20, 8, 4, 20, 100);
+			EmpWage empReliance = new EmpWage("Reliance", 24, 9, 5, 25, 120);
+			EmpWage empBigBasket = new EmpWage("BigBasket", 20, 8, 5, 24, 120);
+	      		System.out.println("Employee Total wage of Company: "+ empDMart.company +" and month wage is: "+empDMart.getTotalEmpSalary());
+			System.out.println("Employee Tatal wage of Company: "+ empReliance.company +" and month wage is: "+ empReliance.getTotalEmpSalary());
+			System.out.println("Employee Tatal wage of Company: "+ empBigBasket.company +" and month wage is: "+ empBigBasket.getTotalEmpSalary());
+	   	}
 	}
-}
+
